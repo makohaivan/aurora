59559200e575edcfa423895f343c73b785dece55
makohaivan <129959379+makohaivan@users.noreply.github.com>
2025-04-23 15:38:34 +0300

    second commit

--- aurora_skin_analyzer/wsgi.py
+++ aurora_skin_analyzer/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aurora_skin_analyzer.settings')
 
 application = get_wsgi_application()
+app = application
